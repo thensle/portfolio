f79cf10349febb8f74678b69357ba0b4e320f7f5
Tara Hensle <tara@northwestern.edu>
2017-07-11 14:49:24 -0500

    created portfolio and blog base pages, modified css, and started server dependencies

---
+++ server.js
@@ -0,0 +1,17 @@
+//DEPENDENCIES
+var bodyParser = require('body-parser');
+var express = require('express');
+var exphbs = require('express-handlebars');
+var mongoose = require('mongoose');
+
+//Custom File Requirements
+
+//Config Express
+
+//BodyParser Config
+
+//MongoDB Config
+
+//Router Link
+
+//Server Listener
