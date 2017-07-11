0572fe092f581a77d6f90fdc424116f37286722c
Tara Hensle <tara@northwestern.edu>
2017-07-11 13:55:34 -0500

    added portfolio html page and created webdev model

---
+++ models/Code.js
@@ -0,0 +1,97 @@
+//Dependencies
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var WebDevSchema = new Schema({
+
+	title: {
+		type: String,
+		trim: true,
+		unique: false,
+		required: false
+	},
+
+	"liveURL": {
+		type: String,
+		trim: true,
+		unique: true,
+		required: true
+	},
+
+	"githubRepo": {
+		type: String,
+		trim: true,
+		unique: true,
+		required: true
+	},
+
+	"tagline": {
+		type: String,
+		trim: true,
+		unique: false,
+		required: true
+	},
+
+	"summary": {
+		type: String,
+		trim: true,
+		unique: false,
+		required: false
+	},
+
+	"image1URL": {
+		type: String,
+		trim: true,
+		unique: false,
+		required: true
+	},
+
+	"image1caption": {
+		type: String,
+		trim: true,
+		unique: false,
+		required: true
+	},
+
+	"image2URL": {
+		type: String,
+		trim: true,
+		unique: false,
+		required: false
+	},
+
+	"image2caption": {
+		type: String,
+		trim: true,
+		unique: false,
+		required: false
+	},
+
+	"image3URL": {
+		type: String,
+		trim: true,
+		unique: false,
+		required: false
+	},
+
+	"image3caption": {
+		type: String,
+		trim: true,
+		unique: false,
+		required: false
+	},
+
+	"technologiesUsed": {
+		type: String,
+		trim: true,
+		unique: false,
+		required: true
+	}
+
+});
+
+
+var WebDev = mongoose.model('WebDev', WebDevSchema);
+
+module.exports = WebDev;
