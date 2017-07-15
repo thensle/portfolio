19a053b56896834cf6fa626eefdc4d3503c74712
Tara Hensle <tara@northwestern.edu>
2017-07-15 13:20:17 -0500

    adjusted server config for Herkou rendering

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var express = require('express');
 var exphbs = require('express-handlebars');
 var mongoose = require('mongoose');
 
-//Custom File Requirements
-var WebDev = require("./models/Code.js");
+//Static Content
+app.use(express.static(__dirname + "/app"));
 
 //Config Express
 var app = express();
@@ -16,21 +16,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-
-//MongoDB Config
-// var db = process.env.MONGODB_URI || "mongodb://localhost/taraPortfolio";
-
-// mongoose.connect(db, function(error) {
-//   if (error) {
-//     console.log(error);
-//   }
-//   else {
-//     console.log("mongoose connection is successful");
-//   }
-// });
-
-//Static Content
-app.use(express.static(__dirname + "/app"));
 
 //HandleBars Config
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
